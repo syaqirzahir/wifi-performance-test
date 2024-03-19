9db969c0fb07f007d3f695f0719bebee544e06de
Syaqir Zahir <hikigayasyaqir@gmail.com>
2024-03-20 04:47:48 +0800

    First push

---
+++ lib/guest_home_screen.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+
+
+
+class GuestHomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Guest Home'),
+      ),
+      body: Center(
+        child: Text('Welcome, ${'Guest'}!'),
+      ),
+    );
+  }
+}
